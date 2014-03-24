e30e6c75ed44e3e755921dd4ccd933d0a882d70e
Gerry Said <gerrysaid@gmail.com>
2014-03-24 11:41:23 +0100

    Lesson 3-24-2014
    Added animation, blend tree which responds to code.

---
+++ Assets/Scripts/playerController.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+private var anim:Animator;
+
+function Start () {
+	//get the animator component associated with the player
+	anim = GetComponent(Animator);
+}
+
+function Update () {
+	anim.SetBool("WalkingLeft",false);
+	anim.SetBool("WalkingRight",false);
+	
+	//if I am moving left
+	if (Input.GetAxis("Horizontal")<0)
+	{
+		anim.SetBool("WalkingLeft",true);
+	}
+	//if I am moving right
+	if(Input.GetAxis("Horizontal")>0)
+	{
+		anim.SetBool("WalkingRight",true);
+	}
+
+}
